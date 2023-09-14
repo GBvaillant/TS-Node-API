86acfca3b1be536fa1e4ea7506e0979e8cc82f80
GBvaillant <vaillantgabriel@gmail.com>
2023-09-14 18:53:03 -0300

    Implementação jest (teste unitário)

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import bodyParser from 'body-parser'
+import { Application } from 'express'
+import './util/module-alias'
+import { Server } from '@overnightjs/core'
+import { ForecastController } from './controllers/forecast'
+
+export class SetupServer extends Server {
+    constructor(private port = 3800) {
+        super()
+    }
+
+    public init(): void {
+        this.setupExpress()
+        this.setupControllers()
+    }
+
+    private setupControllers(): void {
+        const forecastController = new ForecastController()
+        this.addControllers([forecastController])
+    }
+
+    private setupExpress(): void {
+        this.app.use(bodyParser.json())
+    }
+
+    public getApp(): Application {
+        return this.app
+    }
+}
+
