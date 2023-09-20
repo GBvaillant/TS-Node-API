d82f910543ebe28a2295ca724a1ea253f529c636
GBvaillant <vaillantgabriel@gmail.com>
2023-09-19 21:34:12 -0300

    Axios get, request StormGlass

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,10 +1,15 @@
 import { AxiosStatic } from 'axios';
 
 export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection%2CswellHeight%2CswellPeriod%2CwaveDirection%2CwaveHeight%2CwindDirection%2CwindSpeed';
+  readonly stormGlassAPISource = 'noaaa';
+
   constructor(protected request: AxiosStatic) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    this.request.get('')
-    return Promise.resolve({})
+    return this.request.get(
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`
+    );
   }
 }
